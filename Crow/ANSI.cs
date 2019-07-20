97b5ded26108f1bef9583cece90fb9355f48d659
KernelError <villainkernelerror@gmail.com>
2019-07-20 18:11:27 +0200

    Add utility class helping with ANSI formatting

---
+++ Crow/ANSI.cs
@@ -0,0 +1,20 @@
+﻿namespace Crow
+{
+    public static class ANSI
+    {
+        public static string Underline(string input)
+        {
+            return $"\u001B[4m{input}\u001B[0m";
+        }
+
+        public static string Bold(string input)
+        {
+            return $"\u001B[1m{input}\u001B[0m";
+        }
+
+        public static string Reverse(string input)
+        {
+            return $"\u001B[7m{input}\u001B[0m";
+        }
+    }
+}
