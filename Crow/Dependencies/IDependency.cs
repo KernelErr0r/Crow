aef1e0063db55b88b4f28e37a88b22d92c68da50
KernelError <villainkernelerror@gmail.com>
2019-07-23 00:17:21 +0200

    Add abstraction for repositories and dependencies

---
+++ Crow/Dependencies/IDependency.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Crow.Dependencies
+{
+    public interface IDependency
+    {
+        bool HasReleases();
+        void GetRelease(string id, string targetDirectory);
+        Task GetReleaseAsync(string id, string targetDirectory);
+        void DownloadFiles(string id, string targetDirectory);
+        Task DownloadFilesAsync(string id, string targetDirectory);
+    }
+}
---
+++ Crow/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Crow.Repositories
+{
+    public interface IRepository
+    {
+        List<IRepository> GetDependenciesByName(string name);
+        Task<List<IRepository>> GetDependenciesByNameAsync(string name);
+    }
+}
