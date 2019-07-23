6f1d385ffb15dbdec3e2baf807171fd6cc189708
KernelError <villainkernelerror@gmail.com>
2019-07-23 02:47:25 +0200

    Add LocalRepository and LocalDependency

---
+++ Crow/Dependencies/LocalDependency.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Crow.Dependencies
+{
+    public class LocalDependency : IDependency
+    {
+        public string Repository { get; }
+
+        private string filePath;
+
+        public LocalDependency(string filePath, string repository)
+        {
+            this.filePath = filePath;
+            this.Repository = repository;
+        }
+
+        public void DownloadFiles(string id, string targetDirectory)
+        {
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+            var extension = Path.GetExtension(filePath);
+            var path = fileNameWithoutExtension + (String.IsNullOrWhiteSpace(id) ? "" : $"-{id}" + extension);
+
+            File.Copy(path, Path.Combine(targetDirectory, path));
+        }
+
+        public async Task DownloadFilesAsync(string id, string targetDirectory)
+        {
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+            var extension = Path.GetExtension(filePath);
+            var path = fileNameWithoutExtension + (String.IsNullOrWhiteSpace(id) ? "" : $"-{id}" + extension);
+
+            await Task.Run(() =>
+            {
+                File.Copy(path, Path.Combine(targetDirectory, path));
+            });
+        }
+
+        public void GetRelease(string id, string targetDirectory)
+        {
+            DownloadFiles(id, targetDirectory);
+        }
+
+        public async Task GetReleaseAsync(string id, string targetDirectory)
+        {
+            await DownloadFilesAsync(id, targetDirectory);
+        }
+
+        public bool HasReleases()
+        {
+            return File.Exists(filePath);
+        }
+    }
+}
---
+++ Crow/Repositories/LocalRepository.cs
@@ -0,0 +1,58 @@
+﻿using Crow.Dependencies;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Crow.Repositories
+{
+    class LocalRepository : IRepository
+    {
+        public string Name => Path.GetDirectoryName(path);
+
+        private string path;
+
+        public LocalRepository(string path)
+        {
+            if(Directory.Exists(path))
+            {
+                this.path = path;
+            } else
+            {
+                throw new DirectoryNotFoundException(path);
+            }
+        }
+
+        public List<IDependency> GetDependenciesByName(string name)
+        {
+            var list = new List<IDependency>();
+
+            foreach(var file in Directory.GetFiles(path))
+            {
+                if(file.StartsWith(name + "."))
+                {
+                    list.Add(new LocalDependency(file, Name));
+                }
+            }
+
+            return list;
+        }
+
+        public async Task<List<IDependency>> GetDependenciesByNameAsync(string name)
+        {
+            return await Task.Run(() =>
+            {
+                var list = new List<IDependency>();
+
+                Parallel.ForEach(Directory.GetFiles(path), (file) =>
+                {
+                    if (file.StartsWith(name + "."))
+                    {
+                        list.Add(new LocalDependency(file, Name));
+                    }
+                });
+
+                return list;
+            });
+        }
+    }
+}
