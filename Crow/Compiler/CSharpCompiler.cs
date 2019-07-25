9c8b51f26334c8c729e4b8d420573db0255e3ddb
KernelError <villainkernelerror@gmail.com>
2019-07-26 01:39:18 +0200

    Add CSharpCompiler

---
+++ Crow/Compiler/CSharpCompiler.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Crow.Compiler
+{
+    public class CSharpCompiler : ICompiler
+    {
+        public event EventHandler<int> Finished;
+
+        public void Compile(string output, string[] source, string[] libraries)
+        {
+            if(source.Length > 0)
+            {
+                var syntaxTrees = new List<SyntaxTree>();
+                var references = new List<MetadataReference>();
+
+                foreach(var file in source)
+                {
+                    syntaxTrees.Add(CSharpSyntaxTree.ParseText(File.ReadAllText(file)));
+                }
+
+                foreach(var library in libraries)
+                {
+                    references.Add(MetadataReference.CreateFromFile(library));
+                }
+
+                var compilation = CSharpCompilation.Create(output, syntaxTrees, references);
+
+                using(var memoryStream = new MemoryStream())
+                {
+                    var result = compilation.Emit(memoryStream);
+
+                    using(var streamWriter = new StreamWriter(output))
+                    {
+                        streamWriter.Write(memoryStream.ToArray());
+                    }
+                }
+            } else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
