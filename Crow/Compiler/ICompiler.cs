66c704506d8383ba06dabf4c0df93903571c0d02
KernelError <villainkernelerror@gmail.com>
2019-07-26 01:35:56 +0200

    Add interface ICompiler

---
+++ Crow/Compiler/ICompiler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Crow.Compiler
+{
+    public interface ICompiler
+    {
+        event EventHandler<int> Finished;
+
+        void Compile(string output, string[] sourceFiles, string[] libraries);
+    }
+}
