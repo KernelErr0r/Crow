083c44fbb549fc1f7c2e58ab394e4f2660aa9c47
KernelError <villainkernelerror@gmail.com>
2019-07-26 02:03:05 +0200

    Add abstraction for commands

---
+++ Crow/Commands/CommandManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crow.Commands
+{
+    public class CommandManager
+    {
+        public IReadOnlyList<ICommand> Commands => commands;
+
+        private List<ICommand> commands = new List<ICommand>();
+
+        public void Register(ICommand command)
+        {
+            commands.Add(command);
+        }
+
+        public void Unregister(ICommand command)
+        {
+            commands.Remove(command);
+        }
+
+        public void Invoke(string name, params string[] arguments)
+        {
+            foreach(var command in commands)
+            {
+                if(command.Name.ToLower() == name.ToLower())
+                {
+                    command.Invoke(arguments);
+
+                    return;
+                }
+            }
+
+            throw new Exception($"Command {name} doesn't exist");
+        }
+    }
+}
---
+++ Crow/Commands/ICommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Crow.Commands
+{
+    public interface ICommand
+    {
+        string Name { get; }
+        string Description { get; }
+
+        void Invoke(params string[] arguments);
+    }
+}
