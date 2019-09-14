b5e6d7ac3d5eef4afd5e610101ebd7ed27859f5d
KernelError <villainkernelerror@gmail.com>
2019-09-14 21:07:02 +0200

    Add string and number (int, long, etc) parser
    Signed-off-by: KernelError <villainkernelerror@gmail.com>

---
+++ Crow/Commands/Parsers/StringParser.cs
@@ -0,0 +1,15 @@
+using System;
+using Crow.Api.Commands;
+
+namespace Crow.Commands.Parsers
+{
+    [TypeParser(typeof(string), argumentsMax: Int32.MaxValue, priority: 0)]
+    public class StringParser
+    {
+        public bool CanParse(Type type)
+            => true;
+        
+        public string ParseString(string input) 
+            => input;
+    }
+}
