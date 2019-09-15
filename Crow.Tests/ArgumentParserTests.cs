19f6cf8dcf3af6059da30516a08a68916498f8a4
KernelError <villainkernelerror@gmail.com>
2019-09-15 13:35:26 +0200

    Add tests for optional parameters
    Signed-off-by: KernelError <villainkernelerror@gmail.com>

--- Crow.Tests/ArgumentParserTests.cs
+++ Crow.Tests/ArgumentParserTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using Crow.Commands;
 using Crow.Commands.Parsers;
@@ -33,8 +34,43 @@ namespace Crow.Tests
             Assert.Single(parsedArguments);
             Assert.Equal(15, parsedArguments[0]);
         }
+
+        [Fact]
+        public void TestOptionalParameters1()
+        {
+            var argumentParser = new ArgumentParser();
+            var method = GetType().GetMethod("ExampleMethod3", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            argumentParser.RegisterTypeParser(new StringParser());
+
+            var parsedArguments = argumentParser.Parse(method, "test1",  "test2");
+            
+            Assert.Equal(2, parsedArguments.Count);
+            Assert.Equal("test1", parsedArguments[0]);
+            Assert.Equal("test2", parsedArguments[1]);
+            
+            method.Invoke(this, parsedArguments.ToArray());
+        }
+        
+        [Fact]
+        public void TestOptionalParameters2()
+        {
+            var argumentParser = new ArgumentParser();
+            var method = GetType().GetMethod("ExampleMethod3", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            argumentParser.RegisterTypeParser(new StringParser());
+
+            var parsedArguments = argumentParser.Parse(method, "test1");
+            
+            Assert.Equal(2, parsedArguments.Count);
+            Assert.Equal("test1", parsedArguments[0]);
+            Assert.Equal(Type.Missing, parsedArguments[1]);
+
+            method.Invoke(this, parsedArguments.ToArray());
+        }
         
         private void ExampleMethod1(string arg1, string arg2) {}
         private void ExampleMethod2(int arg1) {}
+        private void ExampleMethod3(string arg1, string arg2 = "") {}
     }
 }
