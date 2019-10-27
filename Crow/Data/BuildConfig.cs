7d80e259d75d944440732a9a2d3588ea9c031852
KernelError <villainkernelerror@gmail.com>
2019-10-27 15:10:00 +0100

    Add default values to BuildConfig
    Signed-off-by: KernelError <villainkernelerror@gmail.com>

--- Crow/Data/BuildConfig.cs
+++ Crow/Data/BuildConfig.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Crow.Data
 {
     [Serializable]
     public struct BuildConfig
     {
+        [DefaultValue("Awesome project")]
         public string Name { get; set; }
+        [DefaultValue("1.0.0")]
         public string Version { get; set; }
+        [DefaultValue(".Crow/builds/{VERSION}/{ID}")]
+        public string BuildPath { get; set; }
         
         public List<Compiler> Compilers { get; set; }
         public List<Repository> Repositories { get; set; }
