4bf158869d35a3698308e5e4fec541fccfcf249c
KernelError <villainkernelerror@gmail.com>
2019-10-27 15:47:51 +0100

    Redirect stdout and stderr in CustomCompiler
    Signed-off-by: KernelError <villainkernelerror@gmail.com>

--- Crow/Compiler/CustomCompiler.cs
+++ Crow/Compiler/CustomCompiler.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using Crow.Api.Compiler;
+using Salem;
 
 namespace Crow.Compiler
 {
@@ -12,6 +13,7 @@ namespace Crow.Compiler
         public event EventHandler<int> Finished;
 
         private Process process = new Process();
+        private Logger logger = new Logger("Compiler");
         private string filePath;
         private string arguments;
 
@@ -39,11 +41,32 @@ namespace Crow.Compiler
             var processStartInfo = new ProcessStartInfo()
             {
                 FileName = filePath,
-                Arguments = String.Format(arguments, output, String.Join(' ', sourceFiles))
+                Arguments = String.Format(arguments, output, String.Join(' ', sourceFiles)),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
             };
 
             process.StartInfo = processStartInfo;
+            process.OutputDataReceived += (sender, args) =>
+            {
+                if (!string.IsNullOrWhiteSpace(args.Data))
+                {
+                    logger.Log("Info", args.Data);
+                }
+            };
+            process.ErrorDataReceived += (sender, args) =>
+            {
+                if (!string.IsNullOrWhiteSpace(args.Data))
+                {
+                    logger.Log("Error", args.Data);
+                }
+            };
             process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+            process.Close();
         }
 
         public void Compile(string output, string[] files, string[] libraries)
@@ -51,11 +74,30 @@ namespace Crow.Compiler
             var processStartInfo = new ProcessStartInfo()
             {
                 FileName = filePath,
-                Arguments = String.Format(arguments, output, String.Join(' ', files), String.Join(' ', libraries))
+                Arguments = String.Format(arguments, output, String.Join(' ', files), String.Join(' ', libraries)),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
             };
 
             process.StartInfo = processStartInfo;
+            process.EnableRaisingEvents = true;
+            process.OutputDataReceived += (sender, args) =>
+            {
+                logger.Log("Info", args.Data);
+            };
+            process.ErrorDataReceived += (sender, args) =>
+            {
+                if (!string.IsNullOrWhiteSpace(args.Data))
+                {
+                    logger.Log("Error", args.Data);
+                }
+            };
             process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+            process.Close();
         }
     }
 }
