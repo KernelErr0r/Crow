46398e3ca19eaf3e3964a5445382bca953e73f6d
KernelError <villainkernelerror@gmail.com>
2019-11-16 23:29:19 +0100

    Don't serialize nulls
    Signed-off-by: KernelError <villainkernelerror@gmail.com>

--- Crow/Configs/HjsonConfigSerializer.cs
+++ Crow/Configs/HjsonConfigSerializer.cs
@@ -7,6 +7,8 @@ namespace Crow.Configs
 {
     public class HjsonConfigSerializer : IConfigSerializer
     {
+        private readonly JsonSerializerSettings settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+    
         public void SerializeToFile<T>(string file, T source)
         {
             File.WriteAllText(file, Serialize(source));
@@ -14,7 +16,7 @@ namespace Crow.Configs
 
         public string Serialize<T>(T source)
         {
-            var json = JsonConvert.SerializeObject(source, Formatting.Indented);
+            var json = JsonConvert.SerializeObject(source, Formatting.Indented, settings);
             var hjson = JsonValue.Parse(json).ToString(Stringify.Hjson);
 
             return hjson;
